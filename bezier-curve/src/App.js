ac1afb26ccdc7f8be46cdbaf4ca3801ce1c58e30
TazakiN <tazkia.nizami@gmail.com>
2024-03-15 22:05:06 +0700

    feat: add CtrlPointRenderer and fix bugs

--- bezier-curve/src/App.js
+++ bezier-curve/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import "./App.css";
-import { Mafs, Line, Coordinates, useMovablePoint } from "mafs";
+import { Mafs, Coordinates } from "mafs";
 import BesierLogic from "./BesierLogic.js";
-import DotRenderer from "./DotRenderer.js";
+import ResultRenderer from "./ResultRenderer.js";
+import CtrlPointRenderer from "./CtrlPointRenderer.js";
 
 function App() {
   const [pointNumber, setPointNumber] = useState(3);
@@ -10,7 +11,6 @@ function App() {
   const [inputx, setInputx] = useState([]);
   const [inputy, setInputy] = useState([]);
   const [result, setResult] = useState([]);
-  // const [rerender, setRerender] = useState(false);
 
   const handleInputChangex = (index, value) => {
     const newInputs = [...inputx];
@@ -24,10 +24,6 @@ function App() {
     setInputy(newInputs);
   };
 
-  // const handleAddInput = () => {
-  //   setInputs([...inputs, ""]);
-  // };
-
   const handleBesierLogic = () => {
     const newresult = BesierLogic(pointNumber, iterNumber, inputx, inputy);
     console.log(newresult);
@@ -39,100 +35,101 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
-      <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossorigin
-      ></link>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Stardos+Stencil:wght@400;700&family=Tauri&display=swap"
-        rel="stylesheet"
-      ></link>
-      <h1>Beziers Curve</h1>
-      <br />
-      <div className="input-group">
-        <label htmlFor="titleInput" className="label">
-          Iteration:
-        </label>
-        <input
-          className="input"
-          type="number"
-          placeholder="Number"
-          value={iterNumber}
-          onChange={(e) => {
-            const newValue = parseInt(e.target.value);
-            if (isNaN(newValue)) {
-              setIterNumber(0);
-            } else {
-              setIterNumber(newValue);
-            }
-          }}
-        />
-      </div>
-      <div className="input-group">
-        <label htmlFor="titleInput" className="label">
-          N Points:
-        </label>
-        <input
-          className="input"
-          type="number"
-          placeholder="Number"
-          value={pointNumber}
-          onChange={(e) => {
-            const newValue = parseInt(e.target.value);
-            if (isNaN(newValue)) {
-              setPointNumber(0);
-            } else {
-              setPointNumber(newValue);
-            }
-          }}
-        />
-      </div>
-      <br />
-      {[...Array(pointNumber)].map((_, index) => (
-        <div className="inputRow" key={index}>
-          <div className="input-group">
-            <label className="label">Point {index + 1}</label>
-            <input
-              className="input"
-              type="text"
-              placeholder={`x${index + 1}`}
-              value={inputx[index] ? inputx[index] : ""}
-              onChange={(e) =>
-                handleInputChangex(index, parseInt(e.target.value), 0)
+    <>
+      <div className="container">
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Stardos+Stencil:wght@400;700&family=Tauri&display=swap"
+          rel="stylesheet"
+        ></link>
+        <h1>Beziers Curve</h1>
+        <br />
+        <div className="input-group">
+          <label htmlFor="titleInput" className="label">
+            Iteration:
+          </label>
+          <input
+            className="input"
+            type="number"
+            placeholder="Number"
+            value={iterNumber}
+            onChange={(e) => {
+              const newValue = parseInt(e.target.value);
+              if (isNaN(newValue)) {
+                setIterNumber(0);
+              } else {
+                setIterNumber(newValue);
               }
-            />
-            <input
-              className="input"
-              type="text"
-              placeholder={`y${index + 1}`}
-              value={inputy[index] ? inputy[index] : ""}
-              onChange={(e) =>
-                handleInputChangey(index, parseInt(e.target.value), 1)
-              }
-            />
-          </div>
-          <br />
+            }}
+          />
         </div>
-      ))}
-      {/* <button className="addButton" onClick={handleAddInput}>
-        Add Input
-      </button> */}
-      <button className="addButton" onClick={handleProcessBezierCurve}>
-        Process Bezier Curve
-      </button>
-      <Mafs
-        viewBox={{ x: [-10, 10], y: [-10, 10] }}
-        preserveAspectRatio={false}
-      >
-        <Coordinates.Cartesian />
-        {result.slice(0, result.length - 1).map((_, idx) => (
-          <DotRenderer key={idx} data={result} indek={idx} />
+        <div className="input-group">
+          <label htmlFor="titleInput" className="label">
+            N Points:
+          </label>
+          <input
+            className="input"
+            type="number"
+            placeholder="Number"
+            value={pointNumber}
+            onChange={(e) => {
+              const newValue = parseInt(e.target.value);
+              if (isNaN(newValue)) {
+                setPointNumber(0);
+              } else {
+                setPointNumber(newValue);
+              }
+            }}
+          />
+        </div>
+        <br />
+        {[...Array(pointNumber)].map((_, index) => (
+          <div className="inputRow" key={index}>
+            <div className="input-group">
+              <label className="label">Point {index + 1}</label>
+              <input
+                className="input"
+                type="number"
+                placeholder={`x${index + 1}`}
+                value={inputx[index]}
+                onChange={(e) => handleInputChangex(index, e.target.value, 0)}
+              />
+              <input
+                className="input"
+                type="number"
+                placeholder={`y${index + 1}`}
+                value={inputy[index] ? inputy[index] : ""}
+                onChange={(e) => handleInputChangey(index, e.target.value, 1)}
+              />
+            </div>
+            <br />
+          </div>
         ))}
-      </Mafs>
-    </div>
+
+        <button className="addButton" onClick={handleProcessBezierCurve}>
+          Process Bezier Curve
+        </button>
+      </div>
+      <div className="container" hidden={result.length === 0}>
+        <Mafs
+          viewBox={{ x: [-10, 10], y: [-10, 10] }}
+          preserveAspectRatio={false}
+        >
+          <Coordinates.Cartesian />
+
+          <CtrlPointRenderer listX={inputx} listY={inputy} />
+
+          {result.slice(0, result.length - 1).map((_, idx) => (
+            <ResultRenderer key={idx} data={result} indek={idx} />
+          ))}
+        </Mafs>
+      </div>
+    </>
   );
 }
 
